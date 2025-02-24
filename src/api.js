d8e974303ab817ce6c2507ae3d085cec54a4ec66
Prateek Porwal <prateekporwal456@gmail.com>
2025-02-25 00:42:14 +0530

    codfixing

--- src/api.js
+++ src/api.js
@@ -1,7 +1,11 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyBJyrreTcfnv8vcpASjDtHzz1b48zdJDL8"); // Replace with your API key
+const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+
+const genAI = new GoogleGenerativeAI(API_KEY); // Replace with your API key
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+console.log("API Key:", API_KEY);
 
 export const getChatResponse = async (message) => {
   try {
